60c826978ac3d6960d7c0cf5a640c52a48cf25f9
Shaun2029 <shauns2029@gmail.com>
2015-04-06 23:42:30 +0100

    Added OpenSSH server config defaults.

--- packages/network/openssh/package.mk
+++ packages/network/openssh/package.mk
@@ -63,11 +63,15 @@ post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin/ssh-add
   rm -rf $INSTALL/usr/bin/ssh-agent
   rm -rf $INSTALL/usr/bin/ssh-keyscan
+  
+  mkdir -p $INSTALL/usr/share/services
+    cp -P $PKG_DIR/default.d/*.conf $INSTALL/usr/share/services  
 }
 
 post_install() {
   add_user sshd x 74 74 "Privilege-separated SSH" "/var/empty/sshd" "/bin/sh"
   add_group sshd 74
 
+  enable_service sshd-defaults.service
   enable_service sshd.service
 }
