639d5f2e58e748e13a46f3a5a5a4cea5c05f4026
Shaun2029 <shauns2029@gmail.com>
2016-09-25 17:56:31 +0100

    Apdated HotPi service startup order. Added wget as /bin/wget.real - busybox wget fails to fetch all Adaway sources.

--- packages/hotpi/wget/package.mk
+++ packages/hotpi/wget/package.mk
@@ -16,56 +16,29 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="hotpi"
-PKG_VERSION="1"
+PKG_NAME="wget"
+PKG_VERSION="1.18"
 PKG_REV="1"
-PKG_ARCH="arm"
+PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE=""
-PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain bash dnsmasq bridge-utils avahi openvpn wiringPi wget"
+PKG_SITE="https://ftp.gnu.org"
+PKG_URL="http://ftp.gnu.org/gnu/wget/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain yasm:host"
 PKG_PRIORITY="optional"
-PKG_SECTION="networking"
-PKG_SHORTDESC="Hotpi includes VPN WiFi Hotspot, RadioGaga (Internet Radio) etc."
-PKG_LONGDESC="Hotpi includes VPN WiFi Hotspot, RadioGaga (Internet Radio) etc."
+PKG_SECTION="system"
+PKG_SHORTDESC="Bash is installed to /bin/bash.real"
+PKG_LONGDESC="Bash is a Unix shell written by Brian Fox for the GNU Project as a free software replacement for the Bourne shell."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-pre_configure_target() {
-: # nothing to make here
-}
-
 configure_target() {
-: # nothing to make here
-}
-
-pre_make_target() {
-: # nothing to make here
-}
-
-make_target() {
-: # nothing to make here
+    cd $ROOT/$PKG_BUILD
+    ./configure --host=$TARGET_ARCH --with-ssl=openssl
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL
-  cp -RP $PKG_DIR/files/*  $INSTALL/
-
-  mkdir -p $INSTALL/usr/lib/systemd/system
-    cp $PKG_DIR/system.d/* $INSTALL/usr/lib/systemd/system
-    
+    mkdir -p $INSTALL/bin
+    cp src/wget $INSTALL/bin/wget.real
 }
-
-post_install() {
-  enable_service routing.service
-  enable_service dnsmasq.service
-  enable_service hotspot.service
-  enable_service hotspot-restart.service
-  enable_service hotspot-restart.timer
-  enable_service adaway.service
-  enable_service switch-monitor.service
-  enable_service haveged.service
-}
-
 
